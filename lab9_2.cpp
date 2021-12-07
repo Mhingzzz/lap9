9cf33b0d4af21471962e9f836fdcd76f0df4ad22
mm <nathaphong_p@cmu.ac.th>
2021-12-07 16:07:13 +0700

    q2 finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,27 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double grade) {
+	if (grade > 90)
+	{
+		return 'A';
+	}
+	else if (grade > 75)
+	{
+		return 'B';
+	}
+	else if (grade > 60) 
+	{
+		return 'C';
+	}
+	else if ( grade > 45)
+	{
+		return 'D';
+	}
+	else{
+		return 'F';
+	}
+}
 
 int main(){
 	//Input the number of students
